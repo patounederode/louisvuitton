1446eca5d31a1d10b5b016c11713ae2cd201a9c9
patounederode <153190236+patounederode@users.noreply.github.com>
2025-11-04 18:01:41 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,29 @@
-// JavaScript Document
 console.log("hi");
+// spreekt open button aan
+var openButton = document.querySelector("header button");
+
+openButton.onclick = openMenu;
+
+function openMenu() {  
+
+  var deNav = document.querySelector("nav");
+  // voegt class toe aan nav
+  deNav.classList.add("toonMenu");
+}
+
+
+// menu sluiten
+
+// spreekt sluit button aan
+var sluitButton = document.querySelector("nav button");
+
+sluitButton.onclick = sluitMenu;
+
+// verwijdert de class van de nav
+function sluitMenu() {
+  var deNav = document.querySelector("nav");
+  deNav.classList.remove("toonMenu");
+}
+
+
+
